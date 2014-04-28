9f573a7849a0bda0c07b664cd6dabf4d704881b4
Kerry Channing <cericoda@altfa.com>
2014-04-28 11:56:51 +0100

    enable use of admin to add user

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/users/admin.py
@@ -4,11 +4,24 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 from .models import User
+from django.contrib.auth import forms
 
 
-class UserAdmin(AuthUserAdmin):
-    create_form_class = UserCreationForm
-    update_form_class = UserChangeForm
+class MyUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
 
+    class Meta(UserCreationForm.Meta):
+        model = User
 
-admin.site.register(User, UserAdmin)
+class MyUserAdmin(AuthUserAdmin):  
+    add_form = MyUserCreationForm   
+
+admin.site.register(User,MyUserAdmin)
